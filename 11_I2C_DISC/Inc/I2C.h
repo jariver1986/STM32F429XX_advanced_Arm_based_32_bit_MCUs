19b2b79f4573073ef78ff6857daacc79d5ad648d
John Rivera <62295761+jariver1986@users.noreply.github.com>
2023-08-28 00:27:54 -0500

    Add files via upload

---
+++ 11_I2C_DISC/Inc/I2C.h
@@ -0,0 +1,13 @@
+/*
+ * I2C.h
+ *
+ *  Created on: Aug 13, 2023
+ *      Author: HP VICTUS
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+
+
+#endif /* I2C_H_ */
---
+++ 11_I2C_DISC/Src/usart.c
@@ -0,0 +1,8 @@
+/*
+ * usart.c
+ *
+ *  Created on: Aug 13, 2023
+ *      Author: HP VICTUS
+ */
+
+
